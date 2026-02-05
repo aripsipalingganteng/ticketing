f85ee9aa972fc325b3b2d5a6f6e5ade7d4a8558c
aripganteng <arfmbrq@gmail.com>
2026-02-05 11:08:49 +0300

    add order detail screen and payment method selection screen

--- lib/ui/home/order_detail_screen.dart
+++ lib/ui/home/order_detail_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ticketing/core/components/button.dart';
 import 'package:ticketing/core/components/space.dart';
 import 'package:ticketing/core/constants/color.dart';
 import 'package:ticketing/core/extensions/idr_currency.dart';
+import 'package:ticketing/core/widgets/payment_method_button.dart';
 import 'package:ticketing/gen/assets/assets.gen.dart';
 import 'package:ticketing/ui/home/model/product_model.dart';
-
 
 class OrderDetailScreen extends StatelessWidget {
   final List<ProductModel> products;
@@ -12,6 +13,10 @@ class OrderDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    int paymentButtonIndex = 0;
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail Pesanan'),
@@ -72,6 +77,97 @@ class OrderDetailScreen extends StatelessWidget {
         },
         separatorBuilder: (context, index) => SpaceHeight(20),
         itemCount: products.length,
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          StatefulBuilder(
+            builder: (context, setState) {
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: PaymentMethodButton(
+                        iconPath: Assets.icons.payment.qris.path,
+                        label: 'QRIS',
+                        isActive: paymentButtonIndex == 0,
+                        onPressed: () => setState(() => paymentButtonIndex = 0),
+                      ),
+                    ),
+                    SpaceWidth(20),
+                    Expanded(
+                      child: PaymentMethodButton(
+                        iconPath: Assets.icons.payment.tunai.path,
+                        label: 'Tunai',
+                        isActive: paymentButtonIndex == 1,
+                        onPressed: () => setState(() => paymentButtonIndex = 1),
+                      ),
+                    ),
+                    SpaceWidth(20),
+                    Expanded(
+                      child: PaymentMethodButton(
+                        iconPath: Assets.icons.payment.transfer.path,
+                        label: 'Transfer',
+                        isActive: paymentButtonIndex == 2,
+                        onPressed: () => setState(() => paymentButtonIndex = 2),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          SpaceHeight(32),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 30,
+                  spreadRadius: 0,
+                  offset: Offset(0, -2),
+                  color: AppColors.black.withOpacity(8 / 100),
+                  blurStyle: BlurStyle.outer,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'order summary',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SpaceHeight(4),
+                      Text(
+                        2000000.currencyFormatRp,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Button.filled(
+                    onPressed: () {},
+                    label: 'process',
+                    borderRadius: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
